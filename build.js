adc7cb84c876577e2fe6b6c193ef7c4c39b66370
Georg Kothmeier <georg.kothmeier@roomle.com>
2017-12-21 14:59:47 +0100

    Add uglify to the mix

--- build.js
+++ build.js
@@ -1,11 +1,24 @@
 const fs = require('fs');
 
-const pathJs = __dirname + '/dist/index.js';
+const checkSize = function (path) {
+    const stats = fs.statSync(path);
+    const fileSizeInBytes = stats.size;
+    return fileSizeInBytes / 1000000.0;
+};
+
+const pathJs = __dirname + '/dist/';
+const indexJs = pathJs + 'index.js';
+const indexMinJs = pathJs + 'index.min.js';
 const rollupCommand = 'node ' + __dirname + '/node_modules/rollup/bin/rollup --f iife -c';
 require('child_process').execSync(rollupCommand);
 
-const stats = fs.statSync(pathJs);
-const fileSizeInBytes = stats.size;
-const fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
+const uglifyCommand = 'node ' + __dirname + '/node_modules/uglify-es/bin/uglifyjs -c -o ' + indexMinJs + ' -- ' + indexJs;
+require('child_process').execSync(uglifyCommand);
 
-console.log('File size ' + fileSizeInMegabytes + ' MB');
+const unminifiedSize = checkSize(indexJs);
+const minifiedSize = checkSize(indexMinJs);
+
+console.log('File size not uglified: ' + unminifiedSize + ' MB');
+console.log('File size uglified: ' + minifiedSize + ' MB');
+console.log('File uglified is ' + ((1 - minifiedSize / unminifiedSize) * 100).toFixed(3) + '% smaller!');
+
